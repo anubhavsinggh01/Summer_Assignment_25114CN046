a4127cc5bc0b5bc038b117855e2f6eb6a766fb10
Anubhav Singh <anubhav.singgh01@gmai.com>
2026-06-02 12:50:23 +0530

    2

--- day1/prog2.cpp
+++ day1/prog2.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main(){
     int n,i;
     cout<<"Enter the number whose table is to be printed: ";
-    cin>>n;
-    for(i=1;i<11;i++)
-    {cout<<n<<" * "<<i<<" = "<<n*i<<endl;}
-    return 0;
+    cin >> n;
+    for (i=1;i<=10;i++){
+        cout << n << "x" << i << "=" << n*i << endl;
+    }
+return 0;
 }

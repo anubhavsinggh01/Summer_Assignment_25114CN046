45dcceaad6820f179a0732ac96bfb4c8012cd73c
Anubhav Singh <anubhav.singgh01@gmai.com>
2026-06-02 12:18:50 +0530

    1

--- day1/prog1.cpp
+++ day1/prog1.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i,n,sum=0;
-    cout<<"Enter the final number: ";
-    cin>>n;
-    for(i=1;i<=n;i++)
-    {sum=sum+i;}
-    cout<<"The sum of natural numbers till "<<n<<" is "<<sum<<"."<<endl;
+    int n, sum=0;
+    cout << "enter the number";
+    cin >> n;
+    for (int i=1; i<=n; i++){
+        sum = sum+i; 
+    }
+    cout << "the sum of first " << n << "natural numbers is " << sum << endl;
+    return 0;
 }

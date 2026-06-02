c415a9cba4d492ad99d87881cf80264fcb5b815c
Anubhav Singh <anubhav.singgh01@gmai.com>
2026-06-02 12:52:42 +0530

    3

--- day1/prog3.cpp
+++ day1/prog3.cpp
@@ -4,8 +4,9 @@ int main(){
     int n,i,f=1;
     cout<<"Enter the number whose factorial is to be printed: ";
     cin>>n;
-    for(i=1;i<=n;i++)
-    {f=f*i;}
-    cout<<"The factorial of "<<n<<" is "<<f<<"."<<endl;
+    for (i=1;i<=n;i++){
+        f=f*i;
+    }
+    cout << "the factorial of the number" << n << "is" << f << endl;
     return 0;
 }
